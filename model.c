4e62ead592d05d9afb244a906e607dd1da467d23
Alan Cristhian <alan.cristh@gmail.com>
2024-10-04 21:16:52 -0300

    Add structures and pointer to remember them.

--- model.c
+++ model.c
@@ -10,7 +10,7 @@ int *ptr;  /* A pointer variable. int is the type of the rvalue or value. */
 /* A object declared outside a function is initialized to zero. */
 /* null pointer = doesn't point to any value */
 ptr = &k;  /* Stores the address of k on the value of ptr. */
-*ptr = 7; /* stores 7 on the value of what ptr is pointin on. So stores 7 in k */
+*ptr = 7; /* stores 7 on the value of what ptr is pointing on. So stores 7 in k */
 printf("%d\n", *ptr); /* prints 7 */
 printf("%p\n", (void *)&ptr); /* prints the address of ptr */
 (void *)&j  /* (void *) is the «address type» of c */
@@ -36,4 +36,15 @@ char *ps;
 *ps = 'a';  /* when you change the pointer, also the target value changes */
 ptr = my_string;
 (*ptr)++; /* increments the value of what's ptr is pointing to (Alan->Blan) */
+struct tag {
+    char lname[20];     /* last name */
+    char fname[20];     /* first name */
+    int age;            /* age */
+    float rate;         /* e.g. 12.75 per hour */
+};
+struct tag *st_ptr;  /* pointer to an struct */
+st_ptr = &my_struct;  /* assign the address of my_struct to st_ptr */
+(*st_ptr).age = 63;     // (*st_ptr) : lookup the struct.
+                        // .age : lookup the field age of the struct.
+str_ptr->age = 63;      // Syntax suggar to do the same than above.
 
